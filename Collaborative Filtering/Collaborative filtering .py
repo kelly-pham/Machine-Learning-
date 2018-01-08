f6c43604cb5f66e440cbee5927fd6628a374df73
Kelly Pham <32173513+kelly-pham@users.noreply.github.com>
2018-01-07 22:02:52 -0800

    Example of correlation between movie genres
    Using Euclidean Distance and Pearson Method

---
+++ Collaborative Filtering/Collaborative filtering .py
@@ -0,0 +1,64 @@
+from recommendation import genres
+from math import sqrt
+import matplotlib.pyplot as plt       # to draw a plot
+
+
+# Calculate distance-based similarity using Euclid distance formular: sqrt((a-b) ** 2)
+def Euclid_distance(preferences, genre1, genre2):
+    share_list={}
+    for item in preferences[genre1]:
+        if item in preferences[genre2]:
+            share_list[item]= 1
+
+    # if they have 0 common return 0
+    if len(share_list)== 0:
+        return 0
+
+    Sum_of_square=sum([pow(preferences[genre1][item] - preferences[genre2][item],2)
+                    for item in preferences[genre1] if item in preferences[genre2]])
+    return 1/(1+float(Sum_of_square))
+
+def Pearson(preferences,genre1 ,genre2):
+    share_list={}
+    for item in preferences[genre1]:
+        if item in preferences[genre2]:
+            share_list[item]= 1
+    if len(share_list) == 0:
+        return 0
+
+
+    n = len(share_list)
+    sum1=sum([preferences[genre1][item] for item in share_list])
+    sum2 = sum([preferences[genre2][item] for item in share_list])
+
+    sum1_of_square=sum([pow(preferences[genre1][item],2) for item in share_list])
+    sum2_of_square=sum([pow(preferences[genre2][item],2) for item in share_list])
+
+    product_sum=sum([preferences[genre1][item] * preferences[genre2][item] for item in share_list])
+
+    #Calculate Pearson
+    number=product_sum - (sum1 * sum2 / n)
+    den=sqrt((sum1_of_square-pow(sum1,2)/n)  * (sum2_of_square -pow(sum2,2)/n))
+
+    if den==0:
+        return 0
+    return number/den
+
+def main():
+    print("\tUsing Euclidean distance method\t")
+    object1=Euclid_distance(genres,'Comedy','Romance')
+    print('Similarity Score between {} and {}: {:f}'.format('Comedy','Romance',object1))
+
+    object2=Euclid_distance(genres,'Romance','Horror')
+    print('Similarity Score between {} and {}: {:f}'.format('Romance', 'Horror', object2))
+
+    print("\tUsing Pearson method\t")
+    object3=Pearson(genres,'Comedy','Horror')
+    print('Similarity Score between {} and {}: {:f}'.format('Comedy', 'Horror', object3))
+
+    object4 = Pearson(genres, 'Romance', 'Horror')
+    print('Similarity Score between {} and {}: {:f}'.format('Romance', 'Horror', object4))
+
+
+if __name__ == '__main__':
+    main()
---
+++ Collaborative Filtering/recommendation.py
@@ -0,0 +1,12 @@
+
+
+# list of recommendation movies based on genres, listed on website www.imdb.com
+
+genres= {'Comedy':{'Jumanji':7.3 , 'Shameless' : 8.7,'Pitch Perfect 3': 6.3,'Lady Bird': 8.1,
+            'Disaster Artist':7.9},
+         'Romance':{'The Greatest Showman':8.0,'The shape of water':8.2,'Call me by your name': 8.4,
+                    'Beauty and the Beast': 7.3,'Pitch Perfect 3': 6.3,'Lady Bird': 8.1,'Shameless' : 8.7},
+         'Horror':{'IT':7.6,'Insidious':6.1,'Get out':7.7,'Mother':6.8,'The Killing of Sacred Deer':7.3,
+                   'Mom and Dad':6.5,'The shape of water':8.0}
+        }
+
